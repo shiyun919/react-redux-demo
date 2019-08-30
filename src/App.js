0f349298331091d179f92ce07bbc3a1a2d70d95c
duyu <c_duoyun@outlook.com>
2019-08-30 14:17:45 +0800

    react+redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+	add1(){
+		this.props.onAdd1()
+	}
+	add2(){
+		this.props.onAdd2()
+	}
+	add1IfOdd(){
+		this.props.onAdd3()
+	}
+	add1After2Sec(){
+		this.props.onAdd4()
+	}
+	
+	render(){
+		return (
+		  <div>
+	你点击了<span id="value">{this.props.value}</span>次  
+	<div id="">
+		<button id="add1" onClick={()=>this.add1()}>+1</button>  
+    <button id="add2" onClick={()=>this.add2()}>+2</button>
+		<button id="add1IfOdd" onClick={()=>this.add1IfOdd()} type="button">如果是单数就+1</button>
+		<button id="add1After2Sec" onClick={()=>this.add1After2Sec()} type="button">两秒钟后+1</button>
+	</div>
+</div>
+		)
+	}
 }
+
+
+
+
+
+
+
+
+//function App() {
+//  return (
+//    <div className="App">
+//      你好
+//    </div>
+//  );
+//}
 
 export default App;
